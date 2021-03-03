a83ef892d82e05c00ff0d8f80ae8f854df2208ad
myportfolio-tech <eduardo@myportfolio.com>
2021-03-03 07:06:06 -0500

    Inntial Server app configuration

---
+++ src/server/server.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const express = require('express');
+const { json } = require('body-parser');
+const app = express();
+
+
+// CONSTANTS FROM ENVIRONMENT 
+const dotenv = require('dotenv').config({path: __dirname+'/./../../.env'});
+const port = process.env.PORT
+
+
+const WEATHERBIT_KEY = process.env.WEATHERBIT_KEY;
+WEATHERBIT_URL = process.env.WEATHERBIT_URL;
+GEONAMES = process.env.GEONAMES;
+PIXABAY = process.env.PIXABAY;
+PIXABAY_URL = process.env.PIXABAY_URL;
+
+
+// APP Configuration 
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname,'../client/views')));
+// app.use(express.static(path.join(__dirname,'../../dist')));
+// app.use(favicon(path.join(__dirname, '../client/views', 'favicon.ico')));
+
+
+
+
+app.listen(port, () => console.log(`listening on port ${port}`));
